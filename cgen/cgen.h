e80689e77b31cccc49a4e191f93abe5a50661ab6
GaoYusong <yusong.gao@gmail.com>
2014-05-23 00:25:16 +0800

    cgen in progress

---
+++ cgen/cgen.h
@@ -0,0 +1,94 @@
+#include <assert.h>
+#include <stdio.h>
+#include <set>
+#include <vector>
+#include <utility>
+#include "emit.h"
+#include "cool-tree.h"
+#include "symtab.h"
+
+enum Basicness     {Basic, NotBasic};
+#define TRUE 1
+#define FALSE 0
+
+class CgenClassTable;
+typedef CgenClassTable *CgenClassTableP;
+
+class CgenNode;
+typedef CgenNode *CgenNodeP;
+
+class CgenClassTable : public SymbolTable<Symbol,CgenNode> {
+private:
+   List<CgenNode> *nds;
+   ostream& str;
+   int stringclasstag;
+   int intclasstag;
+   int boolclasstag;
+
+
+// The following methods emit code for
+// constants and global declarations.
+
+   void code_global_data();
+   void code_global_text();
+   void code_bools(int);
+   void code_select_gc();
+   void code_constants();
+   void code_class_nameTab();
+   void code_class_objTab();
+   void code_class_dispTab();
+   void code_class_protObj();
+
+// The following creates an inheritance graph from
+// a list of classes.  The graph is implemented as
+// a tree of `CgenNode', and class names are placed
+// in the base class symbol table.
+
+   void install_basic_classes();
+   void install_class(CgenNodeP nd);
+   void install_classes(Classes cs);
+   void build_inheritance_tree();
+   void set_relations(CgenNodeP nd);
+public:
+   CgenClassTable(Classes, ostream& str);
+   void code();
+   CgenNodeP root();
+};
+
+
+class CgenNode : public class__class {
+private: 
+   CgenNodeP parentnd;                        // Parent of class
+   List<CgenNode> *children;                  // Children of class
+   Basicness basic_status;                    // `Basic' if class is basic
+                                              // `NotBasic' otherwise
+
+public:
+   CgenNode(Class_ c,
+            Basicness bstatus,
+            CgenClassTableP class_table);
+
+   void add_child(CgenNodeP child);
+   List<CgenNode> *get_children() { return children; }
+   void set_parentnd(CgenNodeP p);
+   CgenNodeP get_parentnd() { return parentnd; }
+   int basic() { return (basic_status == Basic); }
+
+   void code_classname(ostream &s);
+   void code_classobj(ostream &s);
+   void code_dispTab(ostream &s);
+   void code_dispTab(CgenNodeP node, std::set<Symbol>&, std::vector<std::pair<Symbol, Symbol> >&);
+   void code_protObj(ostream &s, int);
+   void get_attrs(std::vector<Symbol> &, std::set<Symbol> &, CgenNodeP);
+};
+
+class BoolConst 
+{
+ private: 
+  int val;
+ public:
+  BoolConst(int);
+  void code_def(ostream&, int boolclasstag);
+  void code_ref(ostream&) const;
+};
+
